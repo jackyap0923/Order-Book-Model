d8d8a1582a4e874139ea6119b687f64c36d13adf
Jackyap0923 <s.l.yap@outlook.com>
2025-07-18 01:56:22 +0100

    Added Order Fill Logic, Liquidity Check and FoK orders. Need to fix matching orders in limit_order func.

--- Order_Book.py
+++ Order_Book.py
@@ -37,11 +37,56 @@ class OrderBook:
         self.bids = sorted(self.bids.keys(), reverse=True)
         self.asks = sorted(self.asks.keys())
 
+    def order_fill_logic(self, orders, quantity):
+        for order in orders:
+            if order.quantity >= quantity: #order can be fully filled
+                order.quantity -= quantity 
+                if order.quantity > 0:
+                    break #Order remains in book but with reduced quantity and exit for
+                elif order.quantity == 0:
+                    orders.popleft() #Remove order if quantity is zero and exit for
+                    break
+                else:
+                    raise ValueError("Order quantity cannot be negative.")
+                    
+            else: #Order is partially filled
+                quantity -= order.quantity 
+                orders.popleft() 
+            
+        return quantity #Return remaining quantity to be filled
+    
+    def liquidity_check(self, book, price_level, quantity):
+        prices_to_fill = []
+         #Checking for all available liquidity at or below price
+        for price in book.keys():
+            orders = book[price]
+            temp_quantity = quantity
+            total_quantity = 0
+            for order in orders:
+                total_quantity += order.quantity
+            #append the price levels where fok can be filled
+            if total_quantity >= temp_quantity:
+                prices_to_fill.append(price)
+                break
+            else:
+                prices_to_fill.append(price)
+                temp_quantity -= total_quantity
+
+        return prices_to_fill, total_quantity
+
     def limit_order(self, side, price, quantity):
         order_id = next(self.order_id_counter)
         order = Order(order_id, side, price, quantity)
-        book = self.bids if side == 'bid' else self.asks
+        book = self.bids if side == 'ask' else self.asks #Looks at opposite side of book to check for matching orders
+
+        #Check if order can be filled or partially filled
+
+
+        #If unable to fill, add to book
+        book = self.bids if side == 'bid' else self.asks #Add to correct side of book
         book[price].append(order)
+
+
 
         self.update_book() #Rearrages the book after adding a new order
 
@@ -61,26 +106,30 @@ class OrderBook:
             
             orders = book[best_price]
 
-            for order in orders:
-                if order.quantity >= quantity: #order can be fully filled
-                    order.quantity -= quantity 
-                    if order.quantity > 0:
-                        print(f"Orders have been filled.")
-                        break #Order remains in book but with reduced quantity and exit for
-                    elif order.quantity == 0:
-                        orders.popleft() #Remove order if quantity is zero and exit for
-                        print(f"Orders have been filled")
-                        break
-                    else:
-                        raise ValueError("Order quantity cannot be negative.")
-                        
-                else: #Partial fill of order 
-                    quantity -= order.quantity 
-                    orders.popleft() 
-                    continue
+            self.order_fill_logic(orders, quantity) #Fill the market order with available orders at market price
 
             if not orders: # If no orders left at this price, remove the price level from the book
                 del book[best_price]
 
         self.update_book() #Rearranges the book after processing market order
 
+        #Order must be filled immeidiately or cancelled. Filled until all improvement prices are filled.
+        def fill_or_kill_order(self, side, price, quantity, limit=False): 
+            book = self.bids if side =='ask' else self.asks
+            
+            if not book:
+                raise ValueError("No order avaialable in market.")
+            if price < book.keys():
+                raise ValueError(f"No orders available at or below price {price}.")
+            
+            fok_prices, 
+            
+            #Raise error if not enough liquidity is present
+            if total_quantity < quantity:
+                raise ValueError(f"Not enough quantity available at or below price {price}.")
+            else:
+                for price in fok_prices:
+                    orders = book[price]
+                    self.order_fill_logic(orders, quantity)
+                    if not orders:
+                        del book[price]
